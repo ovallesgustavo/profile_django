eee463f0a6401ee42d033801545f505aa163ab49
Gustavo Ovalles <ovallesgustavo@gmail.com>
2022-01-13 21:11:55 -0400

    Configuración Inicial del Proyecto

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework.authtoken',
+    
+    'profile_api',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +80,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Python_DB',
+        'USER': 'gustavo',
+        'PASSWORD': 'Magallanes1+',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
